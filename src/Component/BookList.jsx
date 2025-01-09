0d243d0f10fff0631e22b2a1cba2d076530a78e4
Thomas Berhane <thomasberhane@Thomass-MacBook-Pro.local>
2025-01-09 14:05:28 -0800

    books list with search button

--- src/Component/BookList.jsx
+++ src/Component/BookList.jsx
@@ -5,14 +5,23 @@ import { FormControl, Row } from 'react-bootstrap'
 import BookListItem from './BookListItem'
 
 export default class BookList extends Component {
+    state = {
+        search:""
+    }
   render() {
     return (
         <>
         <Row>
-            <FormControl type='search'/>
+            <FormControl 
+            placeholder='search' 
+            value={this.state.value}
+            onChange={(e)=> this.setState({search: e.currentTarget.value.toLowerCase()})}
+             />
         </Row>
         <Row>
-            {Fantasy.map(book=>
+            {Fantasy
+            .filter(book=> book.title.toLowerCase().indexOf(this.state.search) !==-1 || book.category.toLowerCase().indexOf(this.state.search) !== -1 )
+            .map(book=>
                 <BookListItem item={book}/> )}
         </Row>
         </>
